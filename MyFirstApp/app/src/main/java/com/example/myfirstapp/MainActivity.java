54ed7ad50a2b33e12dca05188965fe3735269056
Youlek07 <youlek2404@gmail.com>
2025-09-25 08:11:38 +0200

    AM ToastButton
    Wykonane zadanie z użyciem Toast i Buttona.

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Julian Machowski", Toast.LENGTH_SHORT);
+                toast.setMargin(50, 50);
+                toast.show();
+            }
+        });
+    }
+}
